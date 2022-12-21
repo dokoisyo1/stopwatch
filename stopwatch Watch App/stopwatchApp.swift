bcd679be51a84d068ced1b1725cd4d3d6a40770d
macbookpro <sk_20051201@yahoo.co.jp>
2022-12-22 06:39:34 +0900

    Initial Commit

---
+++ stopwatch Watch App/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch Watch App
+//
+//  Created by syo-sksk on 2022/12/22.
+//
+
+import SwiftUI
+
+@main
+struct stopwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by syo-sksk on 2022/12/22.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
